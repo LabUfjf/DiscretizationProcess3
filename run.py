2928a3dfcf210272d4bd8c74675a29224858ff0d
Rafael Mascarenhas Costa <rafaelmascarenhascosta@gmail.com>
2018-11-13 13:31:10 -0200

    end?

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ Created on Tue Nov 13 11:54:43 2018
 
 import numpy as np
 #import scipy.stats as sp
-from scipy.stats import norm, lognorm
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import scipy.io
@@ -19,6 +19,8 @@ import os.path
 import methodDisc as md
 from mpl_toolkits.mplot3d import Axes3D
 from distAnalyze import diffArea
+from scipy.interpolate import interp1d
+from scipy.stats import norm, lognorm
 
 
 if __name__ == '__main__':
@@ -60,7 +62,7 @@ if __name__ == '__main__':
               inf = lognorm.ppf(inf, sigma, loc = 0, scale = np.exp(mu))
               
     xgrid = np.linspace(inf,sup,ngrid)
-    xgridROI = xgrid
+    xgridROI = xgrid.reshape([ROI,ngrid//ROI])
         
     dx = np.diff(xgrid)[0]
     
@@ -76,5 +78,76 @@ if __name__ == '__main__':
             
         else:
             xest = getattr(md,kind)(data,nest,distribuition,mu,sigma,analitica)
+    
+    
+    YY = sf.pdf(xest,mu, sigma,distribuition)
+    fest = interp1d(xest,YY,kind = interpolator, bounds_error = False, fill_value = (YY[0],YY[-1]))
+    
+    yestGrid = []
+        ytruthGrid = []
+        ytruthGrid2 = []
+        divi = []
+        
+        for i in range(ROI):
+            yestGrid.append([fest(xgridROI[i])])
+            ytruthGrid.append([truth(xgridROI[i],mu,sigma,distribuition)])
+            ytruthGrid2.append([truth1(xgridROI[i],mu,sigma,distribuition)])
+            divi.append(len(np.intersect1d(np.where(xest >= min(xgridROI[i]))[0], np.where(xest < max(xgridROI[i]))[0])))
+
+        diff2 = np.concatenate(abs((np.array(yestGrid) - np.array(ytruthGrid))*dx))
+        #diff2[np.isnan(diff2)] = 0
+        areaROI = np.sum(diff2,1)
+        
+        divi = np.array(divi)   
+        divi[divi == 0] = 1
+        
+        try:
+            probROI = np.mean(np.sum(ytruthGrid2,1),1)
+        except:
+            probROI = np.mean(ytruthGrid2,1)
+        
+        
+        probROIord[kind] = np.sort(probROI)
+        index = np.argsort(probROI)
+        
+        areaROIord[kind] = areaROI[index]
+    
+        area = np.append(area,np.sum(areaROIord[kind]))
+        n = np.append(n,len(probROIord[kind]))
+        div[kind] = divi[index]
+    
+    
+    
+        return area,[probROIord,areaROIord]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
